8affe4630e4696acd1d7dd02749f8bf65036b4e7
fandrade83 <fabianotangelo@gmail.com>
2023-05-04 18:07:04 -0300

    initial commit
    Signed-off-by: fandrade83 <fabianotangelo@gmail.com>

--- ex016/ex016.py
+++ ex016/ex016.py
@@ -1,8 +1,16 @@
 '''
 from math import trunc
 num = float(input('Digite um numero: '))
-print('O Valor digitado foi {} e a sua porção inteira é {}'.format(num, trunc(num)))'''
+print('O Valor digitado foi {} e a sua porção inteira é {}'.format(num, trunc(num)))
+'''
 
+'''
+Exercício Python 16: 
+Crie um programa que leia um número Real qualquer pelo teclado e 
+mostre na tela a sua porção Inteira.
+
+
+'''
 num = float(input('Digite um valor: '))
 print('O valor digitado foi {} e a sua porção inteira é {}'.format(num, int(num)))
 
